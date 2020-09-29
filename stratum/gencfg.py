951edead8ee271be18d159a41aa9433add65a94b
Fernando Farias <fernando.farias@rnp.br>
2020-09-29 09:58:24 -0300

    Update of the configuration files of stratum switch

--- stratum/gencfg.py
+++ stratum/gencfg.py
@@ -18,7 +18,7 @@ port = """singleton_ports {{
   slot: 1
   port: {portnum}
   channel: 1
-  speed_bps: 1000000000
+  speed_bps: 100000000000
   config_params {{
     admin_state: ADMIN_STATE_ENABLED
   }}
@@ -33,7 +33,7 @@ def create_chassis():
 
     config = header.format(name=name, nd=nodeid)
     for i in range(1, int(num)):
-        config = config + port.format(portnum=i, nodeid=nodeid, portname="{}-veth{}".format(name, i))
+        config = config + port.format(portnum=i, nodeid=nodeid, portname="port{}".format(i))
 
     with open('/etc/stratum/chassis-config.txt', 'w') as file:
         file.write(config)
